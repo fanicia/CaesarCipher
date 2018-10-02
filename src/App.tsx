f14a5e4b05c3083dbc0047cc090ad0b7e97f682c
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2018-10-02 21:42:16 +0200

    Start pimping the looks.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import './App.css';
 import logo from './logo.svg';
 
 class App extends React.Component {
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      input: '',
+      bruteForce: false,
+      key:'1'
+    }
+  }
+
+  function encrypt(params:type) {
+    
+  }
+
   public render() {
     return (
       <div className="App">
@@ -11,14 +24,13 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React!!!!</h1>
         </header>
         <div>
-            <form>
-              <p>Text to encrypt/decrypt:</p>
-              <input type="text" name="firstname"/>
-              <p>Key:</p>
-              <input type="text" name="lastname"/>
-            </form>
-            <button className="btn btn-default" value="Encrypt">Encrypt</button>
-            <button className="btn btn-default" value="Decrypt">Decrypt</button>
+          <form>
+            <input type="text" className="form-contents form-input-text" size={100} name="firstname" placeholder="Text to encrypt/decrypt"/>
+            <input type="text" className="form-contents form-key" size={3} maxLength={2} name="lastname" placeholder="Key"/>
+          </form>
+          <button className="btn btn-default button-padding" value="Encrypt">Encrypt</button>
+          <button className="btn btn-default button-padding" value="Decrypt">Decrypt</button>
+          <output id="output-field" className="form-output">Output</output>
         </div>
       </div>
     );
