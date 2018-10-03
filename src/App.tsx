c4ef59abecc97745cbd9680ff28659fc8f7f341b
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2018-10-03 17:56:36 +0200

    Debug tslint. and add an alert to the encrypt button.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import * as React from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IProperties {};
+
+type State = {
+  bruteForce?: boolean,
+  inputText?: string,
+  key?: string
+}
+class App extends React.Component<IProperties, State> {
   constructor(props:any) {
     super(props);
     this.state = {
-      input: '',
-      bruteForce: false,
-      key:'1'
+      bruteForce: false,      
+      inputText: '',
+      key:'0'
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.encrypt = this.encrypt.bind(this);
   }
-
-  function encrypt(params:type) {
-    
-  }
-
+  
   public render() {
     return (
       <div className="App">
@@ -25,15 +30,24 @@ class App extends React.Component {
         </header>
         <div>
           <form>
-            <input type="text" className="form-contents form-input-text" size={100} name="firstname" placeholder="Text to encrypt/decrypt"/>
-            <input type="text" className="form-contents form-key" size={3} maxLength={2} name="lastname" placeholder="Key"/>
+            <input type="text" className="form-contents form-input-text" size={100} name="inputText" placeholder="Text to encrypt/decrypt"
+            onChange={this.handleChange}/>
+            <input type="text" className="form-contents form-key" size={3} maxLength={2} name="key" placeholder="Key"/>
           </form>
-          <button className="btn btn-default button-padding" value="Encrypt">Encrypt</button>
+          <button className="btn btn-default button-padding" value="Encrypt" onClick={this.encrypt} >Encrypt</button>
           <button className="btn btn-default button-padding" value="Decrypt">Decrypt</button>
           <output id="output-field" className="form-output">Output</output>
         </div>
       </div>
     );
+  }
+  
+  private handleChange(event:any) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  private encrypt() {
+    alert("State is now set to: inputText: " + this.state.inputText + "key: " + this.state.key)
   }
 }
 
