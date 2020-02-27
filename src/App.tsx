53b9ecb3dd9b777696729a8088d0bd9b28a805e0
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-02-27 13:30:01 +0100

    Start implementation with hooks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState();
+  const [key, setKey] = useState();
+
+  const handleTextChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+    setText(e.target.value);
+  }
+
+  const handleKeyChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+    setKey(e.target.value);
+  }
+  
+  function encrypt(): any {
+
+  }
+
+  function decrypt(): any {
+
+  }
+
+  function bruteForce(): any {
+
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="content">
+        <form>
+          <input type="text" className="form-contents form-input-text" size={100} name="inputText" placeholder="Input-Tekst"
+          onChange={handleTextChange}/>
+          <input type="text" className="form-contents form-key" size={5} maxLength={2} name="key" placeholder="Nøgle"
+          onChange={handleKeyChange}/>
+        </form>
+        <button className="btn btn-default button-padding" value="Encrypt" onClick={encrypt}>Kryptér!</button>
+        <button className="btn btn-default button-padding" value="Decrypt" onClick={decrypt}>Dekryptér!</button>
+        <button className="btn btn-default button-padding hidden-button" onClick={bruteForce}>Giv mig mulighederne!</button>
+        <output id="output-field" className="form-output"/>
+        <output id="debug-field" className="form-output"/>
+      </div>
     </div>
   );
 }
---
+++ src/CaesarEngine.ts
@@ -0,0 +1,12 @@
+import { exception } from "console";
+
+export class CaesarEngine {
+
+    encrypt(text: string): string {
+        throw exception();
+    }
+
+    decrypt(text: string): string {
+        throw exception();
+    }
+}
