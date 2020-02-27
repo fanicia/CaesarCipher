08f939ae83f9cd97175d01d3308995cbf7bba285
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-02-27 15:54:42 +0100

    Start using scss.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { CaesarEngine } from './CaesarEngine';
 
 export default function App() {
